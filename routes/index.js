6bd478d500a36887f0f42cd12e88ad4ce190c3b1
Jingya Huang <jih201@ucsd.edu>
2017-02-23 02:24:09 -0800

    add the alternative page min

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,13 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
+	projects["min"] = false;
+	console.log("min is false");
+  	res.render('index', projects);
+};
+
+exports.viewMin = function(req, res){
+	projects["min"] = true;
+	console.log("min is true");
   	res.render('index', projects);
 };
